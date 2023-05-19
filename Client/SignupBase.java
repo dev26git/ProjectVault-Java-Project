cd6b0e9a27888d28c4cb8d2070ea9d49d5838a41
Devansh-Singh <devanshsingh262@gmail.com>
2023-05-19 15:46:46 +0530

    Update csv path

--- Client/SignupBase.java
+++ Client/SignupBase.java
@@ -9,11 +9,10 @@ public abstract class SignupBase {
 
     protected static void saveAuthorToCsv(String email, String password) {
         String csv = email + "," + password + "\n";
-        try (FileWriter writer = new FileWriter("/Users/sinner/intellij_projects/Project_Repository_Portal/src/Client/user_details.csv", true)) {
+        try (FileWriter writer = new FileWriter("user_details.csv", true)) {
             writer.write(csv);
         } catch (IOException e) {
             System.err.println("Error writing to CSV file: " + e.getMessage());
         }
     }
 }
-
